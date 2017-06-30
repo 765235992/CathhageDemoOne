db0fe7510e854d778287fd601417e059051bc00f
765235992 <765235992@users.noreply.github.com>
2017-07-01 00:08:53 +0800

    1231
    1
    1

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CathhageDemoOne
+//
+//  Created by Jone on 2017/7/1.
+//  Copyright © 2017年 河南东京Web俱乐部. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
